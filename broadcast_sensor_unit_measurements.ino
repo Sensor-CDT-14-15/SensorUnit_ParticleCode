7cc4f92e881c1b5f8e1cc3e513e75606aa284f3f
Josie <jaeh2@cam.ac.uk>
2015-06-18 17:35:13 +0100

    Add in noise analysis: mean, total, sd, max

--- broadcast_sensor_unit_measurements.ino
+++ broadcast_sensor_unit_measurements.ino
@@ -1,12 +1,15 @@
+#include "math.h"
+
 const int PIR_PIN         = A4;
 const int NOISE_PIN       = A5;
 const int PHOTOMETER_PIN  = A6;
 const int TEMPERATURE_PIN = A7;
+const int ARRAY_LENGTH    = 500;
 
-double temperature_celsius, temperature_voltage, light_voltage, raw_pir_reading, noise_voltage, maximum_noise;
+double temperature_celsius, temperature_voltage, light_voltage, raw_pir_reading, noise_voltage, maximum_noise, average_noise, total_noise, standarddev_noise;
 int thresholded_pir_reading;
 char publishString[40];
-double noise_array[500];
+double noise_array[ARRAY_LENGTH];
 
 bool DEBUG_MODE = false;
 
@@ -47,7 +50,6 @@ void publish_measurements() {
 void measure_pir_and_noise() {
 	thresholded_pir_reading = 0;
 	noise_voltage = 0;
-	maximum_noise = 0;
 
 	for (int i = 0; i < 500; i++) {
 		raw_pir_reading = analogRead(PIR_PIN);
@@ -64,21 +66,26 @@ void measure_pir_and_noise() {
 	}
 }
 
-void noise_max(){
+void noise_analysis( ){
+	maximum_noise = 0;
+	total_noise=0;
+	double variance[ARRAY_LENGTH];
+	double variance_sum = 0;
+
+	for (int i=0; i<ARRAY_LENGTH; i++){
+		if (noise_array[i]>maximum_noise) maximum_noise = noise_array[i];
+		total_noise += noise_array[i];
+	}
+	average_noise = average_noise / ARRAY_LENGTH*1.0;
+
+	for (int i=0; i<ARRAY_LENGTH; i++){
+		variance[i] = noise_array[i] - average_noise;
+	    variance_sum += variance[i]*variance[i];
+	}
+    standarddev_noise = variance_sum/ARRAY_LENGTH;
+   // standarddev_noise = sqrt(standarddev_noise);
 
 }
-
-
-void noise_standarddev(){
-
-}
-
-
-void noise_mean(){
-
-	
-}
-
 
 int set_debug_mode(String debug) {
 	// Convert debug string to lowercase
