085b6ea75389f6e0c2ad4a3e4c0a08bfdf547381
James Manton <ajd.manton@googlemail.com>
2015-07-20 16:30:41 +0100

    Add keys to publish string and insert publishing delay
    Reflashing without the delay in the publishing seems to fail

--- broadcast_sensor_unit_measurements.ino
+++ broadcast_sensor_unit_measurements.ino
@@ -7,7 +7,7 @@ const int TEMPERATURE_PIN = A7;
 const int ARRAY_LENGTH    = 500;
 
 float temperature_celsius, temperature_voltage, light_voltage, raw_pir_reading, noise_voltage, noise_maximum, noise_average, noise_total, noise_variance, noise_sd, presence_percentage, num_consecutive_runs;
-char publishString[40];
+char publishString[255];
 float noise_array[ARRAY_LENGTH];
 int pir_array[ARRAY_LENGTH];
 
@@ -43,9 +43,10 @@ void publish_measurements() {
 
 	light_voltage = analogRead(PHOTOMETER_PIN);
 
-	sprintf(publishString,"%.1f, %.1f, %.1f, %.1f, %.1f", temperature_celsius, light_voltage, noise_maximum, noise_average, noise_variance);
+	sprintf(publishString,"temperature: %.1f, light: %.1f, noise-max: %.1f, noise-avg: %.1f, noise-var: %.1f", temperature_celsius, light_voltage, noise_maximum, noise_average, noise_variance);
 	Spark.publish("measurements", publishString);
-	sprintf(publishString, "%.1f, %.1f", presence_percentage, num_consecutive_runs);
+	delay(500);
+	sprintf(publishString, "presence: %.1f, num-runs: %.1f", presence_percentage, num_consecutive_runs);
 	Spark.publish("measurements", publishString);
 }
 
